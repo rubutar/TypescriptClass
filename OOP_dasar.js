02083b960c6ea37dea4318a12c387bb9f4c8466e
Rudi Butarbutar <89676713+rubutar@users.noreply.github.com>
2026-02-23 20:06:02 +0700

    belajar OOP dengan 4 pilar dasar

--- OOP_dasar.js
+++ OOP_dasar.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 //Game in OOP
 var Player = /** @class */ (function () {
     function Player(name, health) {
@@ -13,16 +28,55 @@ var Player = /** @class */ (function () {
         }
         console.log("".concat(enemy.name, " sisa health: ").concat(enemy.health));
     };
+    Player.prototype.updateHealth = function (health) {
+        if (health === void 0) { health = 10; }
+        this.health -= health;
+    };
+    Player.prototype.suaraPlayer = function () {
+        console.log("".concat(this.name, " bersuara..."));
+    };
     return Player;
 }());
-var player1 = new Player("dino1", 100);
+//inheritance (pewarisan)
+var Mage = /** @class */ (function (_super) {
+    __extends(Mage, _super);
+    function Mage(name, health, mana) {
+        var _this = _super.call(this, name, health) || this;
+        _this.mana = mana;
+        return _this;
+    }
+    Mage.prototype.attack = function (enemy) {
+        console.log("".concat(this.name, " menyerang ").concat(enemy.name));
+        enemy.updateHealth(5);
+        console.log("".concat(enemy.name, " teriak kesakitan"));
+    };
+    Mage.prototype.freeze = function (enemy) {
+        console.log("".concat(this.name, " freeze ").concat(enemy.name));
+        enemy.updateHealth(20);
+        this.mana -= 10;
+        console.log("".concat(enemy.name, " freeze"));
+        console.log("mana tersisa: ".concat(this.mana));
+    };
+    Mage.prototype.suaraPlayer = function () {
+        console.log("".concat(this.name, " bersuara \"saya adalah mage terbaik\""));
+    };
+    return Mage;
+}(Player));
+var Fighter = /** @class */ (function (_super) {
+    __extends(Fighter, _super);
+    function Fighter(name, health) {
+        return _super.call(this, name, health) || this;
+    }
+    Fighter.prototype.suaraPlayer = function () {
+        console.log("".concat(this.name, " bersuara \"saya adalah fighter\""));
+    };
+    return Fighter;
+}(Player));
+var eudora = new Mage("eudora", 100, 40);
+var zilong = new Fighter("zilong", 100);
 var player2 = new Player("dino2", 100);
-player1.attack(player2);
-player1.attack(player2);
-player1.attack(player2);
-player1.attack(player2);
-player1.attack(player2);
-player1.attack(player2);
-player1.attack(player2);
-player1.attack(player2);
-player1.attack(player2);
+// eudora.freeze(player2);
+// eudora.attack(player2);
+// zilong.suaraPlayer();
+eudora.attack(zilong);
+console.log(zilong);
--- OOP_dasar.ts
+++ OOP_dasar.ts
@@ -11,23 +11,65 @@ class Player {
         console.log(`${this.name} menyerang ${enemy.name}`);
         enemy.health -= 10;
         console.log(`${enemy.name} teriak kesakitan`);
+        
         if (enemy.health <= 20) {
             console.log(`${enemy.name} mati...`);
         }
         console.log(`${enemy.name} sisa health: ${enemy.health}`);
     }
+    updateHealth(health: number = 10) {
+        this.health -= health;
+    }
+    suaraPlayer() {
+        console.log(`${this.name} bersuara...`);
+    }
 }
 
-let player1 = new Player("dino1", 100);
+//inheritance (pewarisan)
+class Mage extends Player {
+    private mana: number;
+    constructor(name: string, health: number, mana: number) {
+        super(name, health);
+        this.mana = mana;
+    }
+
+    attack(enemy: Player) {
+        console.log(`${this.name} menyerang ${enemy.name}`);
+        enemy.updateHealth(5);
+        console.log(`${enemy.name} teriak kesakitan`);
+    }
+
+    freeze(enemy:Player) {
+        console.log(`${this.name} freeze ${enemy.name}`);
+        enemy.updateHealth(20);
+        this.mana -= 10;
+        console.log(`${enemy.name} freeze`);
+        console.log(`mana tersisa: ${this.mana}`);
+    }
+    suaraPlayer() {
+        console.log(`${this.name} bersuara "saya adalah mage terbaik"`);
+    }
+}
+
+class Fighter extends Player {
+    constructor(name: string, health: number) {
+        super(name, health);
+    }
+
+    suaraPlayer() {
+        console.log(`${this.name} bersuara "saya adalah fighter"`);
+    }
+}
+
+
+
+
+let eudora = new Mage("eudora", 100, 40);
+let zilong = new Fighter("zilong", 100);
 let player2 = new Player("dino2", 100);
 
-
-player1.attack(player2)
-player1.attack(player2)
-player1.attack(player2)
-player1.attack(player2)
-player1.attack(player2)
-player1.attack(player2)
-player1.attack(player2)
-player1.attack(player2)
-player1.attack(player2)
+// eudora.freeze(player2);
+// eudora.attack(player2);
+// zilong.suaraPlayer();
+eudora.attack(zilong);
+console.log(zilong);
